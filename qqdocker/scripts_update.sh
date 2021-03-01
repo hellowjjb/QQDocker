ad32af49bc2f5a41bf7a4b0fc8ef5cdd54cc33a1
hellowjjb <56705018+hellowjjb@users.noreply.github.com>
2021-03-01 14:07:26 +0800

    Update scripts_update.sh

--- qqdocker/scripts_update.sh
+++ qqdocker/scripts_update.sh
@@ -3,7 +3,7 @@ set -e
 
 ######################################获取docker构建文件里面的自定义信息方法-start#####################################################
 function getDockerImageLabel() {
-    repo=akyakya/pack_some_script
+    repo=hellowjjb/qqdocker
     imageTag=latest
     token=$(curl -s "https://auth.docker.io/token?service=registry.docker.io&scope=repository:${repo}:pull" | jq -r '.token')
     digest=$(curl -s -H "Accept: application/vnd.docker.distribution.manifest.v2+json" -H "Authorization: Bearer $token" "https://registry-1.docker.io/v2/${repo}/manifests/${imageTag}" | jq .config.digest -r)
